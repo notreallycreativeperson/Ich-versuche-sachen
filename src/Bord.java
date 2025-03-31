c2d9255b4bdf49a1a9005baac1c6dd17a12f8346
notreallycreativeperson <moritz.sfr@yahoo.com>
2025-03-31 09:14:10 +0200

    Initial commit

--- src/Bord.java
+++ src/Bord.java
@@ -1,2 +1,33 @@
 public class Bord {
+
+    int[][] tiles;
+    boolean isMaxTurn;
+    int[] moves;
+    int moveCount;
+    int[][] last8Moves = new int[2][8];
+
+    public boolean isWon(){
+        return isWon(this);
+    }
+
+    public boolean isFinished(){
+        return isFinished(this);
+    }
+
+    public boolean winningPossible(){
+        return winningPossible(this);
+    }
+
+    public static boolean isWon(Bord bord){
+
+    }
+
+    public static boolean isFinished(Bord bord){
+
+    }
+
+    public static boolean winningPossible(Bord bord){
+
+    }
+
 }
